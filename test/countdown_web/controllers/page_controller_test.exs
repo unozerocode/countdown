7f9ce9315fb5b99c580bc0444c2b58d82830b764
Uno Zero Code <57536442+unozerocode@users.noreply.github.com>
2019-11-14 21:33:41 -0600

    page controller test

--- test/countdown_web/controllers/page_controller_test.exs
+++ test/countdown_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule CountdownWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "[<:F41BBB7A9940C3967F98E2B69C37A15E:>]"
   end
 end
