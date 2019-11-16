8420c4fa68b6e6b56517b6cd10d6575528e3bd63
Uno Zero Code <57536442+unozerocode@users.noreply.github.com>
2019-11-15 20:59:14 -0600

    Use production config from Heroku ENV variables and decrease socket timeout

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,8 @@ use Mix.Config
 # before starting your production server.
 config :countdown, CountdownWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "https://zerocode-countdown.herokuapp.com/", port: 80],
+  url: [scheme: https, host: "https://protected-sierra-57794.herokuapp.com/", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
