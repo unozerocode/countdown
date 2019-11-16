10ccf27a31e536e7de08d960a03b31e43ede5d47
Uno Zero Code <57536442+unozerocode@users.noreply.github.com>
2019-11-15 21:11:39 -0600

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :countdown, CountdownWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [scheme: https, host: "https://protected-sierra-57794.herokuapp.com/", port: 443],
+  url: [scheme: "https", host: "https://protected-sierra-57794.herokuapp.com/", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
